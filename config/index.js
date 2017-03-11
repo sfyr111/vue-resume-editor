a9a5fa2f97b3d80658b2aa285395a10fdd301de3
yangran <sfyr111@163.com>
2017-03-12 01:45:10 +0800

    publish

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-resume-editor/resume/dist', // 这里是压缩dist目录路径
+    assetsPublicPath: '/vue-resume-editor/dist', // 这里是压缩dist目录路径
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
